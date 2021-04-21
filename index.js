b94b763dc183508d8b0f102bc1140ea4175ecb3b
Gabe <djgtraxx@gmail.com>
2021-04-21 02:04:02 +0000

    got the import to a working state.  have not run it against the full set of data yet though, and also, have not tested the login of an imported user, and how the profile edit would act.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var serviceAccount = require("./_firebase-service-account-key.json");
+var otherSecrets  = require("./_other-secrets.json");
 
 require('dotenv').config(); //loads the secrets set in the .env file, into process.env
 const fs = require('fs');
@@ -24,7 +25,7 @@ const db = admin.firestore();
 
 let counter = 0;
 
-fs.createReadStream('_gEditMembers.csv')
+fs.createReadStream('_tmpTestData.csv')
   .pipe(csv())
   .on('data', async (row) => {
     //console.log(`${counter.toString()}.  ${row.firstname} ${row.lastname} <${row.email}>`);
@@ -33,13 +34,16 @@ fs.createReadStream('_gEditMembers.csv')
 
     const strPhotoURL = 'https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png';
 
+    const tmpPW = makeTmpPW(10);
+    console.log(`generated tmpPW: ${tmpPW}`);
+
 
 
     //create the new user
     const createUserResult = await admin.auth().createUser({
       email: row.email,
       emailVerified: false,
-      password: '',
+      password: tmpPW,
       displayName: row.firstname + " " + row.lastname,
       photoURL: strPhotoURL,
       disabled: false
@@ -53,22 +57,22 @@ fs.createReadStream('_gEditMembers.csv')
 
 
           // derive phone number from multiple fields
-          const phoneNumber = (row.phone) ? row.phone : row.custom.businessphone_number;
+          const phoneNumber = (row.phone) ? row.phone : row['custom.business_phone_number'];
 
           // derive country  from multiple fields
           let currentCountry = '';
-          if (custom.business_location.country) {
-            currentCountry = custom.business_location.country;
+          if (row['custom.business_location.country']) {
+            currentCountry = row['custom.business_location.country'];
           }
-          else if (custom.iahsp_country) {
-            currentCountry = custom.iahsp_country;
+          else if (row['custom.iahsp_country']) {
+            currentCountry = row['custom.iahsp_country'];
           }
-          else if (custom.iahsp_location) {
-            currentCountry = custom.iahsp_location;
+          else if (row['custom.iahsp_location']) {
+            currentCountry = row['custom.iahsp_location'];
           }
 
           // normalize the date
-          tmpDateParts = row.membership_expires.split('/');
+          const tmpDateParts = row.membership_expires.split('/');
 
           let dateYYYY = '';
           if (tmpDateParts[2].length <= 2) {
@@ -82,20 +86,25 @@ fs.createReadStream('_gEditMembers.csv')
 
           const dateString = `${dateYYYY}-${dateMM}-${dateDD}`; 
 
-          // normalize the dob
-          tmpDateParts = row.membership_expires.split('/');
-
-          let dateYYYY = '';
-          if (tmpDateParts[2].length <= 2) {
-            dateYYYY = `20${tmpDateParts[2]}`;
+          // normalize the signup date
+          if (row.signup_at) {
+            tmpSignUpStr = row.signup_at;
           } else {
-            dateYYYY = tmpDateParts[2];
+            tmpSignUpStr = row.createdAt;
+          }
+          let tmpSignupDateParts = tmpSignUpStr.split('/');
+
+          let signupDateYYYY = '';
+          if (tmpSignupDateParts[2].length <= 2) {
+            signupDateYYYY = `20${tmpSignupDateParts[2]}`;
+          } else {
+            signupDateYYYY = tmpSignupDateParts[2];
           }
 
-          const dateMM = tmpDateParts[0];
-          const dateDD = tmpDateParts[1];
+          const signupDateMM = tmpSignupDateParts[0];
+          const signupDateDD = tmpSignupDateParts[1];
 
-          const dateString = `${dateYYYY}-${dateMM}-${dateDD}`; 
+          const signupDateString = `${signupDateYYYY}-${signupDateMM}-${signupDateDD}`; 
 
           // derive business name
           // organisation/custom.business_name/company/custom.iahsp_organisation_name
@@ -103,23 +112,23 @@ fs.createReadStream('_gEditMembers.csv')
           if (row.organisation) {
             currentBusinessName = row.organisation;
           }
-          else if (row.custom.business_name) {
-            currentBusinessName = row.custom.business_name;
+          else if (row['custom.business_name']) {
+            currentBusinessName = row['custom.business_name'];
           }
           else if (row.company) {
             currentBusinessName = row.company;
           }
-          else if (row.custom.iahsp_organisation_name) {
-            currentBusinessName = row.custom.iahsp_organisation_name;
+          else if (row['custom.iahsp_organisation_name']) {
+            currentBusinessName = row['custom.iahsp_organisation_name'];
           }
 
           // derive business email
           let currentBusinessEmail = row.email;
-          if (row.custom.business_email) {
-            currentBusinessEmail = row.custom.business_email;
+          if (row['custom.business_email']) {
+            currentBusinessEmail = row['custom.business_email'];
           }
-          else if (row.custom.iahsp_email) {
-            currentBusinessEmail = row.custom.iahsp_email;
+          else if (row['custom.iahsp_email']) {
+            currentBusinessEmail = row['custom.iahsp_email'];
           }
 
           // derive urls
@@ -129,39 +138,39 @@ fs.createReadStream('_gEditMembers.csv')
           let urlInstagram = '';
           let urlPinterest = '';
 
-          if (custom.business_website) {
-            urlWeb = custom.business_website;
+          if (row['custom.business_website']) {
+            urlWeb = row['custom.business_website'];
           }
-          else if (custom.iahsp_website) {
-            urlWeb = custom.iahsp_website;
-          }
-
-          if (custom.business_linkedin) {
-            urlLinkedIn = custom.business_linkedin;
-          }
-          else if (custom.iahsp_linkedin) {
-            urlLinkedIn = custom.iahsp_linkedin;
+          else if (row['custom.iahsp_website']) {
+            urlWeb = row['custom.iahsp_website'];
           }
 
-          if (custom.business_facebook) {
-            urlFacebook = custom.business_facebook;
+          if (row['custom.business_linkedin']) {
+            urlLinkedIn = row['custom.business_linkedin'];
           }
-          else if (custom.iahsp_facebook) {
-            urlFacebook = custom.iahsp_facebook;
-          }
-
-          if (custom.business_instagram) {
-            urlInstagram = custom.business_instagram;
-          }
-          else if (custom.iahsp_instagram) {
-            urlInstagram = custom.iahsp_instagram;
+          else if (row['custom.iahsp_linkedin']) {
+            urlLinkedIn = row['custom.iahsp_linkedin'];
           }
 
-          if (custom.business_pinterest) {
-            urlPinterest = custom.business_pinterest;
+          if (row['custom.business_facebook']) {
+            urlFacebook = row['custom.business_facebook'];
           }
-          else if (custom.iahsp_pinterest) {
-            urlPinterest = custom.iahsp_pinterest;
+          else if (row['custom.iahsp_facebook']) {
+            urlFacebook = row['custom.iahsp_facebook'];
+          }
+
+          if (row['custom.business_instagram']) {
+            urlInstagram = row['custom.business_instagram'];
+          }
+          else if (row['custom.iahsp_instagram']) {
+            urlInstagram = row['custom.iahsp_instagram'];
+          }
+
+          if (row['custom.business_pinterest']) {
+            urlPinterest = row['custom.business_pinterest'];
+          }
+          else if (row['custom.iahsp_pinterest']) {
+            urlPinterest = row['custom.iahsp_pinterest'];
           }
 
 
@@ -181,18 +190,18 @@ fs.createReadStream('_gEditMembers.csv')
             firstName: row.firstname,
             lastName: row.lastname,
             phone: phoneNumber,
-            vatNumber: row.custom.vat_number,
-            address1: row.custom.business_location.line1,
-            address2: row.custom.business_location.line2,
-            city: row.custom.business_location.city,
-            zip: row.custom.business_location.postcode,
+            vatNumber: row['custom.vat_number'],
+            address1: row['custom.business_location.line1'],
+            address2: row['custom.business_location.line2'],
+            city: row['custom.business_location.city'],
+            zip: row['custom.business_location.postcode'],
             countryCustom: currentCountry,
             country: currentCountry,
             isAdmin: false,
             isDisabled: false,
             isApproved: true,
             expiration: dateString,
-            euHomeStagingCourse: row.custom.which_homestaging_course,
+            euHomeStagingCourse: row['custom.which_homestaging_course'],
             description: '',
 
             showPhone: false,
@@ -205,40 +214,34 @@ fs.createReadStream('_gEditMembers.csv')
             urlInstagram: urlInstagram,
             urlPinterest: urlPinterest,
             dob: '',
-            euAffilicatedAssociation: userData.euAffilicatedAssociation,
-            checkboxEthicsCode: userData.checkboxEthicsCode,
-            checkboxStatue: userData.checkboxStatue,
-            checkboxTermsConditions: userData.checkboxTermsConditions,
-            checkboxPrivacyPolicy: userData.checkboxPrivacyPolicy,
-            txtHowFoundUs: userData.txtHowFoundUs,
-            txtYearsInBusiness: userData.txtYearsInBusiness,
-            initialSignUp: userData.initialSignUp,
+            euAffilicatedAssociation: row['custom.national_association'],
+            checkboxEthicsCode: (row['custom.ethics_code']) ? 'true' : '',
+            checkboxStatue: (row['custom.statute']) ? 'true' : '',
+            checkboxTermsConditions: (row.termsandconditions) ? 'true' : '',
+            checkboxPrivacyPolicy: (row.privacypolicy) ? 'true' : '',
+            txtHowFoundUs: row['custom.how_did_you_find_us'],
+            txtYearsInBusiness: row['custom.how_many_years_in_business'],
+            initialSignUp: signupDateString,
 
             // ASP Info
             isASP: false,
             aspid: null
           });
 
-          isSuccess = true;
+          return true;
 
-          finalResults['status'] = isSuccess;
-          finalResults['payload'] = userID;
-
-          return finalResults;
         } else {
           //user creation gracefully failed
-          //console.log(`the .then was executed even though the user was not created...`);
-          finalResults['status'] = false;
-          finalResults['payload'] = null;
-          return finalResults;
+          console.log(`the .then was executed even though the user was not created...`);
+
+          return false;
         }
       })
       .catch((err) => {
         this.currentError = `Error creating User, because: ${err}`;
         console.log(this.currentError);
-        finalResults['status'] = false;
-        finalResults['payload'] = this.currentError;
-        return finalResults;
+
+        return false;;
       }); // admin.auth().createUser()
 
 
@@ -255,20 +258,14 @@ fs.createReadStream('_gEditMembers.csv')
 
 console.log('please wait while the users are imported...');
 
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
+
+const makeTmpPW = (length) => {
+    const result           = [];
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+   }
+   return result.join('');
 }
 
-const start = async () => {
-  await asyncForEach(Object.keys(users), async (idx) => {
-    //await waitFor(50);
-    console.log(idx + ": inserting: " + users[idx].firstName + " " + users[idx].lastName);
-    //await db.collection('users').add(users[idx]);
-    await db.collection("users").doc(idx).set(users[idx]);
-  });
-  console.log('Done');
-}
-
-//start();
